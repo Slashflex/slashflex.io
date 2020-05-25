0b182d632bccfd6d6ed9808878e9fa3e17a9fa6f
slashflex <pro.davidsaoud@gmail.com>
2020-05-25 13:12:22 +0200

    add: profiler package

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
