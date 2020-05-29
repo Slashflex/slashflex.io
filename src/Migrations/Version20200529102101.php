9401eb643c7e8de4d6c5430909c1e503a8180499
slashflex <pro.davidsaoud@gmail.com>
2020-05-29 23:43:00 +0200

    style: forms, loading spinner, nav footer

--- src/Migrations/Version20200529102101.php
+++ src/Migrations/Version20200529102101.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200529065506 extends AbstractMigration
+final class Version20200529102101 extends AbstractMigration
 {
     public function getDescription() : string
     {
