79bc99de59d8b2f97e2650a9b09faa3173d472ad
slashflex <pro.davidsaoud@gmail.com>
2020-06-03 23:17:21 +0200

    add: new migration file (user, article)

--- src/Migrations/Version20200603210806.php
+++ src/Migrations/Version20200603210806.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200603075519 extends AbstractMigration
+final class Version20200603210806 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -26,6 +26,7 @@ final class Version20200603075519 extends AbstractMigration
         $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE project_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE image_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE article_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE role_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE field (id INT NOT NULL, content TEXT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, created_at VARCHAR(255) NOT NULL, login VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
@@ -33,7 +34,8 @@ final class Version20200603075519 extends AbstractMigration
         $this->addSql('CREATE TABLE user_role (user_id INT NOT NULL, role_id INT NOT NULL, PRIMARY KEY(user_id, role_id))');
         $this->addSql('CREATE INDEX IDX_2DE8C6A3A76ED395 ON user_role (user_id)');
         $this->addSql('CREATE INDEX IDX_2DE8C6A3D60322AC ON user_role (role_id)');
-        $this->addSql('CREATE TABLE project (id INT NOT NULL, title VARCHAR(255) NOT NULL, introduction TEXT NOT NULL, content_1 TEXT NOT NULL, content_2 TEXT NOT NULL, content_3 TEXT NOT NULL, main_image VARCHAR(255) NOT NULL, created_at VARCHAR(255) NOT NULL, slug VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE project (id INT NOT NULL, users_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, introduction TEXT NOT NULL, content_1 TEXT NOT NULL, content_2 TEXT NOT NULL, content_3 TEXT NOT NULL, main_image VARCHAR(255) NOT NULL, created_at VARCHAR(255) NOT NULL, slug VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_2FB3D0EE67B3B43D ON project (users_id)');
         $this->addSql('CREATE TABLE project_image (project_id INT NOT NULL, image_id INT NOT NULL, PRIMARY KEY(project_id, image_id))');
         $this->addSql('CREATE INDEX IDX_D6680DC1166D1F9C ON project_image (project_id)');
         $this->addSql('CREATE INDEX IDX_D6680DC13DA5256D ON project_image (image_id)');
@@ -41,13 +43,27 @@ final class Version20200603075519 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_48A04CC6166D1F9C ON project_field (project_id)');
         $this->addSql('CREATE INDEX IDX_48A04CC6443707B0 ON project_field (field_id)');
         $this->addSql('CREATE TABLE image (id INT NOT NULL, url VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE article (id INT NOT NULL, users_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, introduction TEXT NOT NULL, content_1 TEXT NOT NULL, content_2 TEXT NOT NULL, content_3 TEXT NOT NULL, main_image VARCHAR(255) NOT NULL, created_at VARCHAR(255) NOT NULL, slug VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_23A0E6667B3B43D ON article (users_id)');
+        $this->addSql('CREATE TABLE article_image (article_id INT NOT NULL, image_id INT NOT NULL, PRIMARY KEY(article_id, image_id))');
+        $this->addSql('CREATE INDEX IDX_B28A764E7294869C ON article_image (article_id)');
+        $this->addSql('CREATE INDEX IDX_B28A764E3DA5256D ON article_image (image_id)');
+        $this->addSql('CREATE TABLE article_field (article_id INT NOT NULL, field_id INT NOT NULL, PRIMARY KEY(article_id, field_id))');
+        $this->addSql('CREATE INDEX IDX_2C4237497294869C ON article_field (article_id)');
+        $this->addSql('CREATE INDEX IDX_2C423749443707B0 ON article_field (field_id)');
         $this->addSql('CREATE TABLE role (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('ALTER TABLE user_role ADD CONSTRAINT FK_2DE8C6A3A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE user_role ADD CONSTRAINT FK_2DE8C6A3D60322AC FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EE67B3B43D FOREIGN KEY (users_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE project_image ADD CONSTRAINT FK_D6680DC1166D1F9C FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE project_image ADD CONSTRAINT FK_D6680DC13DA5256D FOREIGN KEY (image_id) REFERENCES image (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE project_field ADD CONSTRAINT FK_48A04CC6166D1F9C FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE project_field ADD CONSTRAINT FK_48A04CC6443707B0 FOREIGN KEY (field_id) REFERENCES field (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E6667B3B43D FOREIGN KEY (users_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE article_image ADD CONSTRAINT FK_B28A764E7294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE article_image ADD CONSTRAINT FK_B28A764E3DA5256D FOREIGN KEY (image_id) REFERENCES image (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE article_field ADD CONSTRAINT FK_2C4237497294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE article_field ADD CONSTRAINT FK_2C423749443707B0 FOREIGN KEY (field_id) REFERENCES field (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema) : void
@@ -57,15 +73,22 @@ final class Version20200603075519 extends AbstractMigration
 
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE project_field DROP CONSTRAINT FK_48A04CC6443707B0');
+        $this->addSql('ALTER TABLE article_field DROP CONSTRAINT FK_2C423749443707B0');
         $this->addSql('ALTER TABLE user_role DROP CONSTRAINT FK_2DE8C6A3A76ED395');
+        $this->addSql('ALTER TABLE project DROP CONSTRAINT FK_2FB3D0EE67B3B43D');
+        $this->addSql('ALTER TABLE article DROP CONSTRAINT FK_23A0E6667B3B43D');
         $this->addSql('ALTER TABLE project_image DROP CONSTRAINT FK_D6680DC1166D1F9C');
         $this->addSql('ALTER TABLE project_field DROP CONSTRAINT FK_48A04CC6166D1F9C');
         $this->addSql('ALTER TABLE project_image DROP CONSTRAINT FK_D6680DC13DA5256D');
+        $this->addSql('ALTER TABLE article_image DROP CONSTRAINT FK_B28A764E3DA5256D');
+        $this->addSql('ALTER TABLE article_image DROP CONSTRAINT FK_B28A764E7294869C');
+        $this->addSql('ALTER TABLE article_field DROP CONSTRAINT FK_2C4237497294869C');
         $this->addSql('ALTER TABLE user_role DROP CONSTRAINT FK_2DE8C6A3D60322AC');
         $this->addSql('DROP SEQUENCE field_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
         $this->addSql('DROP SEQUENCE project_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE image_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE article_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE role_id_seq CASCADE');
         $this->addSql('DROP TABLE field');
         $this->addSql('DROP TABLE "user"');
@@ -74,6 +97,9 @@ final class Version20200603075519 extends AbstractMigration
         $this->addSql('DROP TABLE project_image');
         $this->addSql('DROP TABLE project_field');
         $this->addSql('DROP TABLE image');
+        $this->addSql('DROP TABLE article');
+        $this->addSql('DROP TABLE article_image');
+        $this->addSql('DROP TABLE article_field');
         $this->addSql('DROP TABLE role');
     }
 }
