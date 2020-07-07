feef34bb628440cc33609c7195ea57573b88c1f6
slashflex <pro.davidsaoud@gmail.com>
2020-07-07 23:48:22 +0200

    add: elfinder + ckeditor (Working)

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
 ];
