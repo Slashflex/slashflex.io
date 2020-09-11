47d9e93d9d2c0e8714b01ad491a4800df4ebe1b0
slashflex <pro.davidsaoud@gmail.com>
2020-09-11 10:21:11 +0200

    tweak to css color variables

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -46,7 +46,7 @@ $(document).ready(() => {
     })();
   }
 
-  $("#wrapper").mousemove(function (e) {
+  $("#wrapper").mousemove((e) => {
     parallaxIt(e, ".keyF", -40);
     parallaxIt(e, ".keyU", 20);
     parallaxIt(e, ".keyK", -45);
@@ -54,19 +54,20 @@ $(document).ready(() => {
   });
 
   const parallaxIt = (e, target, movement) => {
-    var $this = $("#wrapper");
-    var relX = e.pageX - $this.offset().left;
-    var relY = e.pageY - $this.offset().top;
+    var wrapper = $("#wrapper");
+    var relX = e.pageX - wrapper.offset().left;
+    var relY = e.pageY - wrapper.offset().top;
 
     TweenMax.to(target, 1, {
-      x: ((relX - $this.width() / 2) / $this.width()) * movement,
-      y: ((relY - $this.height() / 2) / $this.height()) * movement,
+      x: ((relX - wrapper.width() / 2) / wrapper.width()) * movement,
+      y: ((relY - wrapper.height() / 2) / wrapper.height()) * movement,
     });
   };
 
   // Swiper
   var swiper = new Swiper(".swiper-container", {
     loop: true,
+    grabCursor: true,
     pagination: {
       el: ".swiper-pagination",
       type: "progressbar",
@@ -116,18 +117,23 @@ $(document).ready(() => {
   };
 
   // Full screen navigation toggle
-  $("#menu-toggle").click(function () {
+  $("#menu-toggle").click(() => {
     $(this).toggleClass("active");
     if ($(this).hasClass("active")) {
       $(".nav").css("position", "static");
     } else {
       $(".nav").css("position", "fixed");
     }
+
+    if (!$('#menu').hasClass('open')) {
+      $(".nav").css("position", "static");
+    } 
+      
     $("#menu").toggleClass("open");
   });
 
   // Flash message
-  setInterval(function () {
+  setInterval(() => {
     $(".flash-notice").slideUp().fadeOut();
   }, 3000);
 
@@ -160,14 +166,44 @@ $(document).ready(() => {
     );
   }
 
+  if (window.location.pathname.includes("/works")) {
+    // $(".swiper-slide").css('width', '0px');
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 600) {
+        $(".swiper-button-next, .swiper-button-prev").css({
+          top: "95%",
+          right: "0",
+        });
+        $(".swiper-slide").css({
+          "box-shadow": "0px 0px 0px white",
+          "border-radius": "25px",
+        });
+      } else {
+        $(".swiper-button-next, .swiper-button-prev").css({
+          top: "50%",
+          right: "0",
+        });
+        $(".swiper-slide").css({
+          "box-shadow": "0px 0px 0px white",
+          "border-radius": "0px",
+        });
+      }
+    });
+  }
+
   if (window.location.pathname == "/works") {
     for (let i = 1; i < 15; i++) {
       let back = document.querySelector(`.back${[i]}`);
-      $(back).css("background-color", "#27282c");
+      $(back).css("background-color", black);
     }
     //   $(".round").css("background-color", "#ffbe41");
   }
 });
+
+const yellow = "#ffbe41";
+const black = "#27282c";
+const grey = "#585858";
+const white = "#fff";
 
 if (
   window.location.pathname == "/" ||
@@ -185,6 +221,15 @@ if (
   );
 }
 
+if (window.location.pathname == "/works") {
+  $('.card-content--image').css('background-size', 'cover');
+}
+// Gooey button on https://slashflex.io/blog
+if (window.location.pathname.includes("/blog")) {
+  $(".button--bubble").css("color", black);
+  $(".button").css("background-color", yellow);
+}
+
 // Loader with random quotes
 if (
   window.location.pathname == "/" ||
@@ -193,36 +238,33 @@ if (
   window.location.pathname == "/blog" ||
   window.location.pathname.includes("/blog/post")
 ) {
-  var sentence = [
-    '“Knowledge is power.” – <i style="color: #585858; font-size: 1.9rem">Francis Bacon</i>',
-    '“Simplicity is the soul of efficiency.” – <i style="color: #585858; font-size: 1.9rem">Austin Freeman</i>',
-    '“Make it work, make it right, make it fast.” – <i style="color: #585858; font-size: 1.9rem">Kent Beck</i>',
-    '“Talk is cheap. Show me the code.“ – <i style="color: #585858; font-size: 1.9rem">Linus Torvalds</i>',
-    '“Software is like sex: It’s better when it’s free.“ – <i style="color: #585858; font-size: 1.9rem">Linus Torvalds</i>',
-    '“The Internet? Is that thing still around?” - <i style="color: #585858; font-size: 1.9rem">Homer Simpson</i>',
-    '“I find your lack of faith disturbing.” - <i style="color: #585858; font-size: 1.9rem">Darth Vader</i>',
-    '“Do. Or do not. There is no try.” - <i style="color: #585858; font-size: 1.9rem">Yoda</i>',
-    '“We meet again at last.” - <i style="color: #585858; font-size: 1.9rem">Darth Vader</i>',
+  const sentence = [
+    `“Knowledge is power.” – <i class="loading__sentence" >Francis Bacon</i>`,
+    `“Simplicity is the soul of efficiency.” – <i class="loading__sentence" >Austin Freeman</i>`,
+    `“Make it work, make it right, make it fast.” – <i class="loading__sentence" >Kent Beck</i>`,
+    `“Talk is cheap. Show me the code.“ – <i class="loading__sentence" >Linus Torvalds</i>`,
+    `“Software is like sex: It’s better when it’s free.“ – <i class="loading__sentence" >Linus Torvalds</i>`,
+    `“The Internet? Is that thing still around?” - <i class="loading__sentence" >Homer Simpson</i>`,
+    `“I find your lack of faith disturbing.” - <i class="loading__sentence" >Darth Vader</i>`,
+    `“Do. Or do not. There is no try.” - <i class="loading__sentence" >Yoda</i>`,
+    `“We meet again at last.” - <i class="loading__sentence" >Darth Vader</i>`,
   ]; // Get a random index from the array sentence
-  var randomNumber = Math.floor(Math.random() * sentence.length); // Get a random sentence from randomNumber
+  const randomNumber = Math.floor(Math.random() * sentence.length); // Get a random sentence from randomNumber
 
-  var quote = sentence[randomNumber];
+  const quote = sentence[randomNumber];
   $("body").append(
-    '<div style="width: 100%; background: #27282c; color: #fff; margin-bottom: 40vh; height: 100%; position: fixed;" id="loading"><div class="loader"></div></div>'
+    `<div style="width: 100%; background: ${black}; color: ${white}; margin-bottom: 40vh; height: 100%; position: fixed;" id="loading"><div class="loader"></div></div>`
   ); // Displays a random quote inside the loading div
 
   $("#loading").append(
-    '<p class="text-center h1" style="color: #ffbe41; margin-bottom: 33rem;">'.concat(
-      quote,
-      "</p>"
-    )
+    `<p class="text-center h1 loading__text">`.concat(quote, "</p>")
   );
-  $(window).on("load", function () {
+  $(window).on("load", () => {
     setTimeout(removeLoader, 600); // Wait for page load
   });
 
-  var removeLoader = function removeLoader() {
-    $("#loading").fadeOut(500, function () {
+  const removeLoader = () => {
+    $("#loading").fadeOut(500, () => {
       // FadeOut complete. Remove the loading div
       $(this).remove(); // Makes page more lightweight
     });
@@ -235,7 +277,6 @@ if (
 
     var tl = new TimelineLite();
     var tl2 = new TimelineLite();
-
     var btTl = new TimelineLite({ paused: true });
 
     tl.to($circlesTopLeft, 1.2, {
@@ -344,4 +385,119 @@ if (
       btTl.restart();
     });
   });
+}
+
+// const collision = ($fix, $moving) => {
+//   var x1 = $fix.offset().left,
+//     y1 = $fix.offset().top,
+//     h1 = $fix.outerHeight(true),
+//     w1 = $fix.outerWidth(true),
+//     b1 = y1 + h1,
+//     r1 = x1 + w1,
+//     x2 = $moving.offset().left,
+//     y2 = $moving.offset().top,
+//     h2 = $moving.outerHeight(true),
+//     w2 = $moving.outerWidth(true),
+//     b2 = y2 + h2,
+//     r2 = x2 + w2;
+
+//   if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+//   return true;
+// };
+
+// // Change color when overlaping same color (Logo & Burger menu)
+// // const stylesheet = document.styleSheets[8];
+
+// $(window).scroll(() => {
+//   const all = $(".moving");
+//   const appendToBurger = $("#trigger").css({
+//     width: "5rem",
+//     height: "5rem",
+//     "background-color": yellow,
+//     position: "absolute",
+//     right: "1rem",
+//     top: "1.7rem",
+//   });
+
+//   for (let i = 0; i < all.length; i++) {
+//     if (collision($(".fix"), all.eq(i)) && collision($("#burger"), all.eq(i))) {
+//       $(".fix").css("color", yellow);
+//       console.log("not overlaping");
+//       appendToBurger.css("opacity", 0.7);
+
+//       // appendToBurger.css('opacity', .7);
+//       // $("#burger").css("background", yellow);
+//       // stylesheet.addRule("#burger::before", `background-color: ${yellow}`);
+//       // stylesheet.insertRule(
+//       //   `#burger::before { background-color: ${yellow} }`);
+//       // stylesheet.addRule("#burger::after", `background-color: ${yellow}`);
+//       // stylesheet.insertRule(
+//       //   `#burger::after { background-color: ${yellow} }`);
+//       break;
+//     } else {
+//       $(".fix").css("color", black);
+//       console.log("overlaping");
+//       appendToBurger.css("opacity", "0");
+
+//       // appendToBurger;
+//       // $("#burger").css("background", black);
+//       // stylesheet.addRule("#burger::before", `background-color: ${black}`);
+//       // stylesheet.insertRule(
+//       //   `#burger::before { background-color: ${black} }`);
+//       // stylesheet.addRule("#burger::after", `background-color: ${black}`);
+//       // stylesheet.insertRule(`#burger::after { background-color: ${black} }`);
+//     }
+//   }
+// });
+
+// Dark/Light Theme switch
+const checkbox = document.querySelector("input[name=theme]");
+
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    transition();
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    transition();
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
+
+let transition = () => {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+};
+
+const themeSwitch = document.getElementById("switch");
+if (themeSwitch) {
+  initTheme(); // on page load, if user has already selected a specific theme -> apply it
+
+  themeSwitch.addEventListener("change", function (event) {
+    resetTheme(); // update color theme
+  });
+
+  function initTheme() {
+    var darkThemeSelected =
+      localStorage.getItem("switch") !== null &&
+      localStorage.getItem("switch") === "dark";
+    // update checkbox
+    themeSwitch.checked = darkThemeSelected;
+    // update body data-theme attribute
+    darkThemeSelected
+      ? document.querySelector("html").setAttribute("data-theme", "dark")
+      : document.querySelector("html").removeAttribute("data-theme");
+  }
+
+  function resetTheme() {
+    if (themeSwitch.checked) {
+      // dark theme has been selected
+      document.querySelector("html").setAttribute("data-theme", "dark");
+      localStorage.setItem("switch", "dark"); // save theme selection
+    } else {
+      document.querySelector("html").removeAttribute("data-theme");
+      localStorage.removeItem("switch"); // reset theme selection
+    }
+  }
 }
