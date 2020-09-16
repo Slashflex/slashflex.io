72fd3cc5257f2f2dec9dfc5f3bc30f9614bd6237
slashflex <pro.davidsaoud@gmail.com>
2020-09-16 19:15:56 +0200

    feat: reset password logic

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
