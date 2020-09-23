b4b25421d7fc7782206f69cdecc9334333f5ddfd
slashflex <pro.davidsaoud@gmail.com>
2020-09-23 11:10:26 +0200

    fix to migrations dir

--- src/Migrations/Version20200923090953.php
+++ src/Migrations/Version20200923090953.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200921180033 extends AbstractMigration
+final class Version20200923090953 extends AbstractMigration
 {
     public function getDescription() : string
     {
