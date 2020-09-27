e996a8b6b7535ef3805eef571a96882066d3644e
slashflex <pro.davidsaoud@gmail.com>
2020-09-27 12:53:31 +0200

    feat: guard authenticator

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements PasswordAuthenticatedInterface
+{
+    use TargetPathTrait;
+
+    private $entityManager;
+    private $urlGenerator;
+    private $csrfTokenManager;
+    private $passwordEncoder;
+    private $userRepository;
+
+    /**
+     * @param UserRepository $userRepository
+     * @param EntityManagerInterface $entityManager
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param CsrfTokenManagerInterface $csrfTokenManager
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(UserRepository $userRepository,  EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->entityManager = $entityManager;
+        $this->urlGenerator = $urlGenerator;
+        $this->csrfTokenManager = $csrfTokenManager;
+        $this->passwordEncoder = $passwordEncoder;
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @param Request $request
+     * @return void
+     */
+    public function supports(Request $request)
+    {
+        return 'user_signin' === $request->attributes->get('_route')
+            && $request->isMethod('POST');
+    }
+
+    /**
+     * Get user's credentials
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function getCredentials(Request $request)
+    {
+        $credentials = [
+            'username' => $request->request->get('_username'),
+            'password' => $request->request->get('_password'),
+            'csrf_token' => $request->request->get('_csrf_token'),
+        ];
+        $request->getSession()->set(
+            Security::LAST_USERNAME,
+            $credentials['username']
+        );
+
+        return $credentials;
+    }
+
+    /**
+     * Check for user's credentials
+     *
+     * @param [type] $credentials
+     * @param UserProviderInterface $userProvider
+     * @return void
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $token = new CsrfToken('authenticate', $credentials['csrf_token']);
+        if (!$this->csrfTokenManager->isTokenValid($token)) {
+            throw new InvalidCsrfTokenException();
+        }
+
+        $user = $this->userRepository->findOneBy(['email' => $credentials['username']]);
+
+        if (!$user) {
+            // fail authentication with a custom error
+            throw new CustomUserMessageAuthenticationException('Invalid credentials');
+        }
+
+        return $user;
+    }
+
+    /**
+     * Verify user password and check if token is enabled or not 
+     * eg. if he has clicked on the confirmation link received by mail on sign up
+     *
+     * @param [type] $credentials
+     * @param UserInterface $user
+     * @return void
+     */
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        $email = $this->userRepository->findOneBy(['email' => $user->getUsername()]);
+
+        // Prevents non confirmed account from loging in
+        if ($email->getTokenEnabled() == false) {
+            throw new CustomUserMessageAuthenticationException('You have to confirm your account please');
+            return new RedirectResponse($this->urlGenerator->generate('home'));
+        } else {
+            return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+        }
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     *
+     * @param [type] $credentials
+     * @return string|null
+     */
+    public function getPassword($credentials): ?string
+    {
+        return $credentials['password'];
+    }
+
+    /**
+     * On success redirect to user's profile route
+     *
+     * @param Request $request
+     * @param TokenInterface $token
+     * @return void
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate('user'));
+    }
+
+    /**
+     * Generates user login route
+     *
+     * @return void
+     */
+    protected function getLoginUrl()
+    {
+        return $this->urlGenerator->generate('user_signin');
+    }
+}
